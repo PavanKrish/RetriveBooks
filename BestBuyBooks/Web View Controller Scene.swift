29df2849159fc644e46c50d3ed46a074335a0274
User <User@users-macbook.local>
2016-11-23 13:33:29 -0500

    @ps114 - added various view controllers.

---
+++ BestBuyBooks/Web View Controller Scene.swift
@@ -0,0 +1,35 @@
+//
+//  Web View Controller Scene.swift
+//  BestBuyBooks
+//
+//  Created by User on 11/23/16.
+//  Copyright © 2016 Gelli,Chiklith Madhukar Siva Sai. All rights reserved.
+//
+
+import UIKit
+
+class Web_View_Controller_Scene: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
