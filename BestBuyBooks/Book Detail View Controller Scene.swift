f6cd3d940140086f426017c8d2eea29aaebec83a
User <User@users-macbook.local>
2016-11-28 23:36:10 -0500

    @ps114 - Created Navigation controller and given SegUi to controllers..Created Outlets and Action Results

--- BestBuyBooks/Book Detail View Controller Scene.swift
+++ BestBuyBooks/Book Detail View Controller Scene.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class Book_Detail_View_Controller_Scene: UIViewController {
 
+    @IBOutlet weak var ivBookCover: UIImageView!
+    
+    @IBOutlet weak var lbTitle: UILabel!
+    
+    @IBOutlet weak var lbAuthor: UILabel!
+    @IBOutlet weak var lbPublisher: UILabel!
+    
+    @IBOutlet weak var lbDescription: UITextView!
+    
+    @IBOutlet weak var btnApple: UIButton!
+    
+    @IBOutlet weak var btnGoogle: UIButton!
+    @IBOutlet weak var lbGooglePrice: UILabel!
+    @IBOutlet weak var lbApplePrice: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
