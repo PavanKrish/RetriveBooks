75e785b18a6260fd6670cf9c262e725e493c755e
User <User@users-macbook.local>
2016-11-28 22:54:40 -0500

    @cg90 : Added code for button search , controlling the text ..

--- BestBuyBooks/Search View Controller Scene.swift
+++ BestBuyBooks/Search View Controller Scene.swift
@@ -10,12 +10,36 @@ import UIKit
 
 class Search_View_Controller_Scene: UIViewController {
 
+    @IBOutlet weak var tfQuery: UITextField!
+    var arrayBooks : NSMutableArray!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
+    @IBOutlet weak var onSubmit: UIButton!
+    
+    @IBAction func onSubmit(_ sender: AnyObject) {
+        
+        self.arrayBooks = NSMutableArray()
+        if self.tfQuery.text?.characters.count == 0{
+            let alert = UIAlertController(title: "Alert", message: "query is empty", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            
+            return;
+    }
+        if (self.tfQuery.text?.characters.count)! > 30{
+            let alert = UIAlertController(title: "Alert", message: "Query should be short than 30 characters", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            
+            return;
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
