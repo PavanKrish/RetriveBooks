e8fa79670b38e78108ea747d180fdfc0aad71905
User <User@users-macbook.local>
2016-12-06 00:10:33 -0500

    @va27: Addition of Book Result Cell

---
+++ BestBuyBooks/Book Result Cell.swift
@@ -0,0 +1,26 @@
+//
+//  Book Result Cell.swift
+//  BestBuyBooks
+//
+//  Created by User on 12/5/16.
+//  Copyright © 2016 Gelli,Chiklith Madhukar Siva Sai. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class BookResultCell: UITableViewCell {
+
+
+    @IBOutlet weak var ivBookCover: UIImageView!
+
+    @IBOutlet weak var lbBookTitle: UILabel!
+    
+    @IBOutlet weak var lbBookAuthor: UILabel!
+    
+    @IBOutlet weak var lbBookPublisher: UILabel!
+    
+
+    
+    
+}
--- BestBuyBooks/Result Table View Controller Scene.swift
+++ BestBuyBooks/Result Table View Controller Scene.swift
@@ -3,12 +3,12 @@
 //  BestBuyBooks
 //
 //  Created by User on 11/23/16.
-//  Copyright © 2016 Gelli,Chiklith Madhukar Siva Sai. All rights reserved.
+//  Copyright © 2016 Ammakolla, Viswakaranreddy. All rights reserved.
 //
 
 import UIKit
 
-class Result_Table_View_Controller_Scene: UIViewController {
+class Result_Table_View_Controller_Scene: UITableViewController {
     
     var arrayBook : NSMutableArray! = NSMutableArray()
     var dataBook : Data!
@@ -24,16 +24,35 @@ class Result_Table_View_Controller_Scene: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func numberOfSections(in tableView: UITableView) -> Int {
+   override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+   override  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return self.arrayBook.count
     }
-
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell : BookResultCell = tableView.dequeueReusableCell(withIdentifier: "BookCell", for: indexPath) as! BookResultCell
+        
+        // Configure the cell...
+        let book1 = self.arrayBook.object(at: indexPath.row) as! BookObject
+        
+        if book1.imagelink == "N/A"{
+            cell.ivBookCover.image = UIImage(named: "imagena.png")
+        }else{
+            let url = URL (string: book1.imagelink)
+            cell.ivBookCover.sd_setImage(with: url)
+        }
+        
+        cell.lbBookTitle.text = book1.title
+        cell.lbBookAuthor.text = book1.author
+        cell.lbBookPublisher.text = book1.publisher
+        
+        return cell
+    }
     /*
     // MARK: - Navigation
 
